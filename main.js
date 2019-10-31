848feffab74f6a0f676d9bf5ff0b61834a415ac1
Florencia Lafuente <lafuente.florencia@gmail.com>
2019-10-30 21:09:10 -0300

    HTML structure

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hola');
+function generatePattern () {
+  let rowsNumber = 0;
+  let colsNumber = 0;
+  let colorPalette = [];
+
+  rowsNumber = parseInt(document.getElementById('rowsInput').value);
+  colsNumber = parseInt(document.getElementById('colsInput').value);
+  colorPalette = document.getElementById('colorPaletteSelect').value.split(',');
+
+};
+
+function clearGrid() {
+  console.log('clearGrid');
+};
